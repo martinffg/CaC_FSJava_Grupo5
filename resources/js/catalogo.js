6818678e500d1f0ef5385b4ccc14d921cf4c293f
Martín Fernández <martinffg@gmail.com>
2024-05-26 20:32:22 -0300

    corrección catalogo.js

--- resources/js/catalogo.js
+++ resources/js/catalogo.js
@@ -5,30 +5,15 @@ async function cargarDatos() {
   const response = await fetch("../data/data.json");
   juegos = await response.json();
   mostrarPorPantalla(juegos);
-  //juegos.forEach(insertarCard);
 }
 
 cargarDatos()
-
-// FORMATO GENÉRICO DE LA FUNCIÓN CARGARDATOS CON MANEJO DE ERRORES.
-// function cargarDatos() {
-//   return fetch("./resources/data/data.json")
-//     .then((resolve) => {
-//       return resolve.json();
-//     })
-//     .then((json) => {
-//       mostrarPorPantalla(json)
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// }
 
 function mostrarPorPantalla(juegos) {
   const juegosFiltradosPorDescargas = juegos.filter((objeto) => objeto.descargas > 0);
   juegosFiltradosPorDescargas.forEach(insertarCard);
 }
-  
+
   // OPCIONES CON OTROS FILTROS APLICABLES
   //--------------------------------------------------------------------------
   // const juegosFiltradosPorPrecio = juegos.filter( objeto => {
