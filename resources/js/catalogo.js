bb3464b89852fdd38abe8ec84916daf7d394610d
Martín Fernández <martinffg@gmail.com>
2024-05-26 19:29:03 -0300

    primer commit catalogo con json de prueba

---
+++ resources/js/catalogo.js
@@ -0,0 +1,47 @@
+
+let juegos;
+
+async function cargarDatos() {
+  const response = await fetch("../data/data.json");
+  juegos = await response.json();
+  mostrarPorPantalla(juegos);
+  juegos.forEach(insertarCard);
+}
+
+cargarDatos()
+
+// FORMATO GENÉRICO DE LA FUNCIÓN CARGARDATOS CON MANEJO DE ERRORES.
+// function cargarDatos() {
+//   return fetch("./resources/data/data.json")
+//     .then((resolve) => {
+//       return resolve.json();
+//     })
+//     .then((json) => {
+//       mostrarPorPantalla(json)
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// }
+
+function mostrarPorPantalla(juegos) {
+  const juegosFiltradosPorDescargas = juegos.filter((objeto) => objeto.descargas > 0);
+  juegosFiltradosPorDescargas.forEach(insertarCard);
+}
+  
+  // OPCIONES CON OTROS FILTROS APLICABLES
+  //--------------------------------------------------------------------------
+  // const juegosFiltradosPorPrecio = juegos.filter( objeto => {
+  //   return objeto.precio > 2
+  // });
+  //const juegosFiltradosPorPrecio = juegos.filter((objeto) => objeto.precio > 2);
+  //juegosFiltradosPorPrecio.forEach(insertarCard);
+  // console.log(juegosFiltradosPorPrecio);
+  //--------------------------------------------------------------------------
+  // const encontrarJuego = juegos.find((juego) => {
+  //   return juego.nombre === "Assesin Creed";
+  // });
+  //const encontrarJuego = juegos.find((juego) => juego.nombre === "Assesin Creed");
+  // console.log(encontrarjuego);
+  // juegos.forEach(insertarCard);
+  //--------------------------------------------------------------------------
---
+++ resources/js/catalogo_funciones.js
@@ -0,0 +1,51 @@
+function insertarCard(juego, i, juegos) {
+    const card = crearCard(juego);
+    controlDescargas(card);
+    renderizarCard(card);
+  }
+  
+  function crearCard(juego) {
+    const divCard =document.createElement('div');
+    const img = document.createElement('img');
+  
+    //img.classList.add('catalogo');
+    divCard.classList.add('card');
+  
+    // desestructuración de array
+    // const {nombre, precio, descargas, urlImg}  =   juego;
+    const nombre = juego.nombre;
+    const precio = juego.precio;
+    const descargas = juego.descargas;
+    const urlDownload = juego.urlDownload;
+    const urlImg = juego.urlImg;
+       
+    divCard.innerHTML = `
+       <a href="${juego.urlDownload}"><img src=${juego.urlImg}/></a> 
+       <h2>${nombre}</h2>
+       <p>Precio USD: ${precio}</p>
+       <p>Descargas: ${descargas}</p>`;
+  
+    return divCard;
+  }
+  
+  function controlDescargas(card) {
+    console.log();
+    if(obtenerDescargas(card) < 50){
+      card.classList.add('bajo-descargas')
+    }
+  }
+  
+  function obtenerDescargas(card){
+    let parrafos = card.querySelectorAll('p');
+    let parrafoDescargas = parrafos[1];
+    let contenidoParrafo = parrafoDescargas.textContent
+  
+    let descargasValor = contenidoParrafo.split(':')[1];
+  
+    return parseInt(descargasValor);
+  }
+  
+  function renderizarCard(card){
+    const container = document.querySelector('#catalogo-container');
+    container.appendChild(card);
+  }
